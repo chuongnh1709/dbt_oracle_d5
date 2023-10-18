fe10503e1dbe09671fee851f226f9a2945e68188
Chuong.NguyenH2 <Chuong.NguyenH2@homecredit.vn>
2023-10-18 15:34:19 +0700

    upd test_day model

--- models/mytests/test_day.sql
+++ models/mytests/test_day.sql
@@ -6,9 +6,9 @@
 {% set now_2 = modules.datetime.datetime.today().weekday() %}
 
 {{ log('Day of month is : ' ~ now , info=True) }}
-{{ log('Function return 1 : ' ~ is_today_xday()  , info=True) }}
+{{ log('Function return 1 (dom): ' ~ is_today_xday()  , info=True) }}
 
-{{ log('now_2 is : ' ~ now_2 , info=True) }}
-{{ log('Function return 2 : ' ~ is_today_xday_of_week()  , info=True) }}
+{{ log('weekday is : ' ~ now_2 , info=True) }}
+{{ log('Function return 2 (dow): ' ~ is_today_xday_of_week()  , info=True) }}
 
 select 1 as id from dual
